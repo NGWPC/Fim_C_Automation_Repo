b5de802b555688f3ae2a31ef39b4c71254ce8417
Jyoti Mikkilineni <jyoti.mikkilineni@U-32OHAX6W5U2AH.nextgenwaterprediction.com>
2024-07-03 15:58:21 -0400

    Updated the test case name  and data file name

--- scripts/tests/PI1/test_fim_c_006_subcase2.py
+++ scripts/tests/PI1/test_fim_c_006_subcase2.py
@@ -1,7 +1,7 @@
 import pytest
 from ..helpers import run_commands
 
-def test_fim_c_006_subcase3(test_name,run_shell_script,remove_file): #,change_dir
+def test_fim_c_006_subcase2(test_name,run_shell_script,remove_file): #,change_dir
     folder_name = 'PI1'
     data_file = test_name + '.json'
     dir_path,output_file,expected_data_file = run_shell_script(folder_name,data_file)
