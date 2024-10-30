617cc96ce39591644c26aac5d69ce41741fc5986
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 20:03:03 +0000

    Add new file_Recovering corrupted file

---
+++ scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_C_subcase2.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+from ...helpers import validate_directories_files
+from ...utils import csv_util,tif_util,directory_util
+
+@pytest.mark.skip(reason="skipping this test for now")
+def test_fim_c_EVAL_TP_C_subcase2(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,fetch_directory_details,remove_file):
+    tn = test_name + '.json'
+    folder_name = 'PI3/data'
+    docker_command,dir_paths = fetch_docker_details(folder_name,tn,scenarios)
+    run_docker_script(folder_name,tn,scenarios,docker_command)
