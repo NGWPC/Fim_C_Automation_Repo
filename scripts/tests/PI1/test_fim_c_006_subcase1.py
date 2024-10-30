3a5bbf154cb214dc89a643a5911b2733fa59ed20
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 15:21:46 +0000

    Update test_fim_c_006_subcase1.py

--- scripts/tests/PI1/test_fim_c_006_subcase1.py
+++ scripts/tests/PI1/test_fim_c_006_subcase1.py
@@ -1,11 +1,8 @@
 import pytest
-import os
-from ..helpers import validate_geo_data
+from ..helpers import validate_directories_files
 
-# @pytest.mark.skip(reason="skipping this test for now")
-def test_fim_c_006_subcase2(test_name,run_shell_script,remove_file,scenarios): #,change_dir
+def test_fim_c_006_subcase1(test_name,load_scenario_data,scenarios):
+    tn = test_name + '.json'
     folder_name = 'PI1/data'
-    data_file = test_name + '.json'
-    dir_path,output_file,expected_data_file,data_file_location = run_shell_script(folder_name,data_file,scenarios)
-    validate_geo_data(dir_path,output_file,os.path.expanduser(expected_data_file),os.path.expanduser(data_file_location))
-    remove_file(folder_name,data_file,dir_path,scenarios)
+    directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
+    validate_directories_files(directory_locations,directory_contents)
