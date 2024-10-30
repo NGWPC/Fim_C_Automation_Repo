ccb4f0325384ab305e2979f6b0c6d7016e2d007a
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 20:17:46 +0000

    Add new file_Recovering corrupted file

---
+++ scripts/tests/PI3/RAS/test_fim_c_RAS_001_subcase1.py
@@ -0,0 +1,12 @@
+import pytest
+from ...helpers import validate_directories_files
+from ...utils import sql_util
+
+# @pytest.mark.skip(reason="skipping this test for now")
+def test_fim_c_RAS_001_subcase1(test_name,load_scenario_data,scenarios,sql_read):
+    tn = test_name + '.json'
+    folder_name = 'PI3/data'
+    directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
+    validate_directories_files(directory_locations,directory_contents)
+    dir_path = sql_read(folder_name,tn,scenarios)
+    sql_util.test_sql_read(dir_path)
