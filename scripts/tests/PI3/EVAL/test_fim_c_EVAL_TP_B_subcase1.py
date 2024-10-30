513c82c351d9431d4c936b80236503d29d522526
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 19:56:16 +0000

    Add new file_Recovering corrupted file

---
+++ scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_B_subcase1.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+from ...helpers import validate_directories_files,validate_response
+from ...utils import tif_util,json_util,directory_util
+
+# @pytest.mark.skip(reason="skipping this test for now")
+def test_fim_c_EVAL_TP_B_subcase1(test_name,load_scenario_data,scenarios,run_curl_command):
+    tn = test_name + '.json'
+    folder_name = 'PI3/data'
+    url,headers,data, destination_response_location = run_curl_command(folder_name,tn,scenarios)
+    validate_response(url,headers,data,os.path.expanduser(destination_response_location))
