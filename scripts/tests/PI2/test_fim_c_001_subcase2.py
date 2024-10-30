16cc5803d145a84ff19c3afbab3031d1c4e544c9
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 16:07:44 +0000

    Update test_fim_c_001_subcase2.py_Recovering corrupted file

--- scripts/tests/PI2/test_fim_c_001_subcase2.py
+++ scripts/tests/PI2/test_fim_c_001_subcase2.py
@@ -1,14 +1,20 @@
 import pytest
+import os
 from ..helpers import validate_directories_files,generate_dfo_data
-from ..utils import txt_util,readme_util
+from ..utils import txt_util,readme_util,json_util,csv_util
 
-def test_fim_c_001_subcase2(test_name,load_scenario_data,scenarios,read_me):
+# @pytest.mark.skip(reason="skipping this test for now")
+def test_fim_c_001_subcase2(test_name,load_scenario_data,scenarios,read_me,json_read,read_csv):
     tn = test_name + '.json'
     folder_name = 'PI2/data'
     generate_dfo_data()
     directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
     # print(str(directory_locations[0]))
     # remove_file(folder_name,'report.html',str(directory_locations[0]),scenarios)
-    validate_directories_files(directory_locations,directory_contents)
+    validate_directories_files([os.path.expanduser(each_directory_locations) for each_directory_locations in directory_locations ],directory_contents)
     source_read_me_location,destination_read_me_location = read_me(folder_name,tn,scenarios)
-    readme_util.validate_readme( source_read_me_location,destination_read_me_location)
+    readme_util.validate_readme( os.path.expanduser(source_read_me_location),os.path.expanduser(destination_read_me_location))
+    source_json_file_location,destination_json_file_location = json_read(folder_name,tn,scenarios)
+    json_util.validate_json( os.path.expanduser(source_json_file_location),os.path.expanduser(destination_json_file_location))
+    csv_file,output_csv_file = read_csv(folder_name,tn,scenarios)
+    csv_util.test_extract_csv_data(os.path.expanduser(csv_file),os.path.expanduser(output_csv_file))
