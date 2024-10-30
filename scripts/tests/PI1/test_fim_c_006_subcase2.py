297f120f3476bc13b8f05c54652bbc1119f896b0
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 15:22:40 +0000

    Update test_fim_c_006_subcase2.py_Recovering corrupted file

--- scripts/tests/PI1/test_fim_c_006_subcase2.py
+++ scripts/tests/PI1/test_fim_c_006_subcase2.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from ..helpers import validate_geo_data
 
 # @pytest.mark.skip(reason="skipping this test for now")
@@ -6,5 +7,5 @@ def test_fim_c_006_subcase2(test_name,run_shell_script,remove_file,scenarios): #
     folder_name = 'PI1/data'
     data_file = test_name + '.json'
     dir_path,output_file,expected_data_file,data_file_location = run_shell_script(folder_name,data_file,scenarios)
-    validate_geo_data(dir_path,output_file,expected_data_file,data_file_location)
+    validate_geo_data(dir_path,output_file,os.path.expanduser(expected_data_file),os.path.expanduser(data_file_location))
     remove_file(folder_name,data_file,dir_path,scenarios)
