60ec351fad76347a7c5b184de1bd93724a7059a8
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 15:30:32 +0000

    Update test_fim_c_008_subcase1.py_Recovering corrupted file

--- scripts/tests/PI1/test_fim_c_008_subcase1.py
+++ scripts/tests/PI1/test_fim_c_008_subcase1.py
@@ -1,6 +1,6 @@
 import pytest
-from ..helpers import validate_directories_files
-from ..helpers import validate_geo_data
+import os
+from ..helpers import validate_directories_files,validate_geo_data
 from ..utils import csv_util
 
 def test_fim_c_008_subcase1(test_name,load_scenario_data,scenarios,read_csv,fetch_data_file_details):
@@ -9,7 +9,7 @@ def test_fim_c_008_subcase1(test_name,load_scenario_data,scenarios,read_csv,fetc
     directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
     validate_directories_files(directory_locations,directory_contents)
     csv_file,output_csv_file = read_csv(folder_name,tn,scenarios)
-    csv_util.test_extract_csv_data(csv_file,output_csv_file)
+    csv_util.test_extract_csv_data(csv_file,os.path.expanduser(output_csv_file))
     dir_path,output_file,expected_data_file,data_file_location = fetch_data_file_details(folder_name,tn,scenarios)
-    validate_geo_data(dir_path,output_file,expected_data_file,data_file_location)
+    validate_geo_data(dir_path,output_file,os.path.expanduser(expected_data_file),os.path.expanduser(data_file_location))
  
