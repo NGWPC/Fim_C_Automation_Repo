c6316b89afcc21c8712006edcafb64cf0b7b2ea2
Jyoti Mikkilineni1 <jyotipriya.mikkilineni@ertcorp.com>
2024-10-30 15:38:36 +0000

    Add new file_Recovering corrupted file

---
+++ scripts/tests/PI1/test_fim_c_009_subcase3.py
@@ -0,0 +1,8 @@
+import pytest
+from ..helpers import validate_directories_files
+
+def test_fim_c_009_subcase3(test_name,load_scenario_data,scenarios):
+    tn = test_name + '.json'
+    folder_name = 'PI1/data'
+    directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
+    validate_directories_files(directory_locations,directory_contents)
