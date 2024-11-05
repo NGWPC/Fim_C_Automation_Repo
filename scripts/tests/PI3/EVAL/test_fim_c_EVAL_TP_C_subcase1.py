d11a1360c4d7bb30fbb2cd95b9a482bc0dd5493d
Parallel Works app-run user <jyoti.mikkilineni@mgmt-jyotimikkilineni-oefimevalvizcopyjyotioe-00045.optimizationuseast1-5.pw.local>
2024-11-05 19:11:36 +0000

    Updating code for tif and json

--- scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_C_subcase1.py
+++ scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_C_subcase1.py
@@ -16,10 +16,10 @@ def test_fim_c_EVAL_TP_C_subcase1(test_name,load_scenario_data,scenarios,fetch_d
     directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
     validate_directories_files([os.path.expanduser(each_directory_locations) for each_directory_locations in directory_locations ],directory_contents)
 
-#     tif_source_file,tif_destination_file = read_tif(folder_name,tn,scenarios)
-#     for source_file , destination_file in zip([os.path.expanduser(each_tif_source_file) for each_tif_source_file in tif_source_file],[os.path.expanduser(each_tif_destination_file) for each_tif_destination_file in tif_destination_file ]):
-#           tif_util.test_tif_data(source_file,destination_file)
-#     source_json_file_location,destination_json_file_location = json_read(folder_name,tn,scenarios)
-#     for source_json_file , destination_json_file in zip([os.path.expanduser(each_json_source_file) for each_json_source_file in source_json_file_location],[os.path.expanduser(each_json_destination_file) for each_json_destination_file in destination_json_file_location ]):
-#           json_util.validate_json( os.path.expanduser(source_json_file),os.path.expanduser(destination_json_file))
+    tif_source_file,tif_destination_file = read_tif(folder_name,tn,scenarios)
+    for source_file , destination_file in zip([os.path.expanduser(each_tif_source_file) for each_tif_source_file in tif_source_file],[os.path.expanduser(each_tif_destination_file) for each_tif_destination_file in tif_destination_file ]):
+          tif_util.test_tif_data(source_file,destination_file)
+    source_json_file_location,destination_json_file_location = json_read(folder_name,tn,scenarios)
+    for source_json_file , destination_json_file in zip([os.path.expanduser(each_json_source_file) for each_json_source_file in source_json_file_location],[os.path.expanduser(each_json_destination_file) for each_json_destination_file in destination_json_file_location ]):
+          json_util.validate_json( os.path.expanduser(source_json_file),os.path.expanduser(destination_json_file))
     remove_file(folder_name,tn,os.path.expanduser(directory),scenarios)
--- scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_D_subcase1.py
+++ scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_D_subcase1.py
@@ -9,6 +9,6 @@ def test_fim_c_EVAL_TP_D_subcase1(test_name,load_scenario_data,scenarios,read_ti
     folder_name = 'PI3/data'
     directory_locations,directory_contents = load_scenario_data(folder_name,tn,scenarios)
     validate_directories_files(directory_locations,directory_contents)
-    # tif_source_files,tif_destination_files = read_tif(folder_name,tn,scenarios)
-    # for tif_source_file , tif_destination_file in zip([os.path.expanduser(each_tif_source_file) for each_tif_source_file in tif_source_files ],[os.path.expanduser(each_tif_destination_file) for each_tif_destination_file in tif_destination_files ]):
-    #     tif_util.test_tif_data(os.path.expanduser(tif_source_file),os.path.expanduser(tif_destination_file))
+    tif_source_files,tif_destination_files = read_tif(folder_name,tn,scenarios)
+    for tif_source_file , tif_destination_file in zip([os.path.expanduser(each_tif_source_file) for each_tif_source_file in tif_source_files ],[os.path.expanduser(each_tif_destination_file) for each_tif_destination_file in tif_destination_files ]):
+        tif_util.test_tif_data(os.path.expanduser(tif_source_file),os.path.expanduser(tif_destination_file))
