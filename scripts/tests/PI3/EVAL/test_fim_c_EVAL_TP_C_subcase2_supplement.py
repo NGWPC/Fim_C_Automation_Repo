daac4d1821b55a37f967e157f32e5037cf2c60de
Parallel Works app-run user <jyoti.mikkilineni@mgmt-jyotimikkilineni-oefimevalvizcopyjyotioe-00050.optimizationuseast1-5.pw.local>
2024-12-02 19:30:32 +0000

    Enabling TP_C_Subcase2_supplement test case

--- scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_C_subcase2_supplement.py
+++ scripts/tests/PI3/EVAL/test_fim_c_EVAL_TP_C_subcase2_supplement.py
@@ -2,7 +2,7 @@ import pytest
 import os
 from ...helpers import validate_directories_files,verify_STAC_dashboard
 
-@pytest.mark.skip(reason="skipping this test for now")
+# @pytest.mark.skip(reason="skipping this test for now")
 def test_fim_c_EVAL_TP_C_subcase2_supplement(test_name,load_scenario_data,scenarios):
     # tn = test_name + '.json'
     # folder_name = 'PI3/data'
