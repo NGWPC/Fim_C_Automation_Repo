8924e0de62ec89cb5a76788a1fc2bc4614e1cf3e
Parallel Works app-run user <jyoti.mikkilineni@mgmt-jyotimikkilineni-fimowppi3slurmcopyjyotioe-00005.optimizationuseast1-5.pw.local>
2024-12-08 05:52:33 +0000

    Adding updates to HAND scenario

--- scripts/tests/PI3/HAND/test_fim_c_HAND_TP_A_subcase2.py
+++ scripts/tests/PI3/HAND/test_fim_c_HAND_TP_A_subcase2.py
@@ -4,7 +4,7 @@ import time
 from ...helpers import validate_directories_files,validate_geo_data
 from ...utils import gpkg_metadata_util
 
-# @pytest.mark.skip(reason="skipping this test for now")
+@pytest.mark.skip(reason="skipping this test for now")
 def test_fim_c_HAND_TP_A_subcase2(test_name,load_scenario_data,scenarios,run_shell_script_from_directory):
     tn = test_name + '.json'
     folder_name = 'PI3/data'
