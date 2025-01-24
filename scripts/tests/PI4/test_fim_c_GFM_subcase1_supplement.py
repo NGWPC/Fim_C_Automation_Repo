d888f2321d56cde90b762384c9d61e11097a4ecd
Parallel Works app-run user <jyoti.mikkilineni@mgmt-jyotimikkilineni-oefimbenchmarkstaccopyjyotioe-00017.optimizationuseast1-5.pw.local>
2025-01-24 16:10:13 +0000

    Adding updates to PI4 scenarios

--- scripts/tests/PI4/test_fim_c_GFM_subcase1_supplement.py
+++ scripts/tests/PI4/test_fim_c_GFM_subcase1_supplement.py
@@ -1,7 +1,7 @@
 import pytest
 import os
-from ..helpers import verify_gfm_data
+from ..utils import benchmark_stac_util
 # @pytest.mark.skip(reason="skipping this test for now")
 def test_fim_c_EVAL_TP_C_subcase2_supplement(test_name,load_scenario_data,scenarios):
     print("Main test")
-    verify_gfm_data()
+    benchmark_stac_util.verify_gfm_data()
---
+++ scripts/tests/utils/benchmark_stac_util.py
@@ -0,0 +1,41 @@
+import os
+import requests
+import json
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service #as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+metrics_page_title = (By.XPATH,'//h1[@id="cross-site-contingency-metrics"]')
+metrics_page_link='http://127.0.0.1:3000'
+benchmark_category_field_xpath = '//label[@for="inputs-3a86ea-4"]//b'
+table_header_xpath = '//*[@id="inputs-3a86ea-8"]//table//tr//th[2]'
+benchmark_category_xpath = '//select[@id="inputs-3a86ea-4"]'
+comp_version_xpath = '//select[@id="inputs-3a86ea-9"]'
+plot_type_xpath = '//select[@id="inputs-3a86ea-1"]'
+plot_type_label_xpath = '//*[@for="inputs-3a86ea-1"]/b'
+contingency_metric_xpath = '//select[@id="inputs-3a86ea-2"]'
+# dot_plot_type_xpath = '//*[@class="plot-d6a7b5"]//*[@aria-label="dot"][1]//*[@cx="475"][1]'
+box_plot_type_xpath = '//*[@class="plot-d6a7b5"]//*[@aria-label="bar"][1]//*[@cx="55"]'
+dot_plot_type_xpath = (By.XPATH,'//*[@class="plot-d6a7b5"]//*[@aria-label="dot"][1]//*[@cx="475"][1]')
+
+
+def verify_gfm_data():
+   print('I am here')
+   
+   chrome_options = webdriver.ChromeOptions()
+   chrome_options.binary_location = "/usr/bin/chromium-browser"
+   chrome_options.add_argument("--headless")
+   chrome_options.add_argument("--no-sandbox")
+   chrome_options.add_argument("--disable-dev-shm-usage")
+   print('I am here too')
+
+   driver = webdriver.Chrome(executable_path = ChromeDriverManager().install(),options=chrome_options)
+   driver.get('localhost:8080')
+   page_url = driver.current_url
+   print(page_url)
