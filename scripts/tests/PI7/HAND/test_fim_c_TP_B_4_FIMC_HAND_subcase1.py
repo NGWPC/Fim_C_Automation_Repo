7f8916cb642bb7c43db81d4368cc6afaa3bd2f9a
Jyoti.Mikkilineni <Jyoti.P.Mikkilineni@rtx.com>
2025-11-06 18:46:42 +0000

    Added updates for HAND TP-B subcase1

--- scripts/tests/PI7/HAND/test_fim_c_TP_B_4_FIMC_HAND_subcase1.py
+++ scripts/tests/PI7/HAND/test_fim_c_TP_B_4_FIMC_HAND_subcase1.py
@@ -4,14 +4,14 @@ from ...helpers import validate_directories_files,validate_response,validate_dow
 from ...utils import csv_util,gpkg_util
 
 # @pytest.mark.skip(reason="skipping this test for now")
-def test_fim_c_TP_A_4_FIMC_HAND_subcase1(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,fetch_data_file_details,remove_file):
+def test_fim_c_TP_B_4_FIMC_HAND_subcase1(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,fetch_data_file_details,run_shell_script_from_directory,remove_file):
     tn = test_name + '.json'
     folder_name = 'PI7/data'
     docker_command,dir_paths = fetch_docker_details(folder_name,tn,scenarios)
     run_docker_script(folder_name,tn,scenarios,docker_command)
+    run_shell_script_from_directory(folder_name,tn,scenarios)
     directory_locations,directory_contents,flag = load_scenario_data(folder_name,tn,scenarios)
     validate_directories_files(directory_locations,directory_contents,flag)
-    for dir_path in dir_paths:
-        remove_file(folder_name,tn,dir_path,scenarios)
-        print("File removed")
-        print(dir_path)
+    remove_file(folder_name,tn,dir_paths,scenarios)
+    print("File removed")
+    print(dir_paths)
