7815aef492f333440b6d0ce868c6a23179e99fed
Jyoti.Mikkilineni <Jyoti.P.Mikkilineni@rtx.com>
2025-12-11 21:57:16 +0000

    Adding updates to PI7 EVAL subcase1 data file

--- scripts/tests/PI7/EVAL/test_fim_c_TP_A_EVAL_subcase1.py
+++ scripts/tests/PI7/EVAL/test_fim_c_TP_A_EVAL_subcase1.py
@@ -1,10 +1,10 @@
 import pytest
 import os
 from ...helpers import validate_directories_files,validate_response,validate_downloaded_content,update_aws_credentials
-from ...utils import csv_util,gpkg_util
+from ...utils import csv_util,gpkg_util,txt_file_validation_util
 
 # @pytest.mark.skip(reason="skipping this test for now")
-def test_fim_c_TP_A_EVAL_subcase1(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,read_csv,fetch_data_file_details,remove_file):
+def test_fim_c_TP_A_EVAL_subcase1(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,read_csv,fetch_data_file_details,fetch_txt_details,remove_file):
     tn = test_name + '.json'
     folder_name = 'PI7/data'
     # docker_command,dir_paths = fetch_docker_details(folder_name,tn,scenarios)
@@ -14,10 +14,13 @@ def test_fim_c_TP_A_EVAL_subcase1(test_name,load_scenario_data,scenarios,fetch_d
     docker_commands,dir_paths = fetch_docker_details(folder_name,tn,scenarios)
      
     for i,docker_command in enumerate(docker_commands):
-         if i==1:
+         if i==2:
            docker_command=update_aws_credentials(docker_command)
          run_docker_script(folder_name,tn,scenarios,docker_command)
          print("The command ran successfully.")
+    text,dir_paths = fetch_txt_details(folder_name,tn,scenarios)
+    for txt, dir_path in zip(text,[os.path.expanduser(each_dir_path_file) for each_dir_path_file in dir_paths ]):
+          txt_file_validation_util.txt_file_validation(txt,dir_path)
     # directory_locations,directory_contents,flag = load_scenario_data(folder_name,tn,scenarios)
     # validate_directories_files(directory_locations,directory_contents,flag)
     # csv_file,output_csv_file = read_csv(folder_name,tn,scenarios)
---
+++ scripts/tests/utils/txt_file_validation_util.py
@@ -0,0 +1,6 @@
+
+def txt_file_validation(text,new_file):
+
+    with open(new_file, encoding="utf-8") as f:
+        content = f.read()
+    assert text in content , f"{text} not found"
