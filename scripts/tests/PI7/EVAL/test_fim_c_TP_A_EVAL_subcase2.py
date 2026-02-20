4f65c308acd3d709ecc4ddb457ccdd0ca4a14860
Jyoti.Mikkilineni <Jyoti.P.Mikkilineni@rtx.com>
2026-02-20 19:34:04 +0000

    Adding TP_A_EVAL_subcase2 updates

---
+++ scripts/tests/PI7/EVAL/test_fim_c_TP_A_EVAL_subcase2.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+from ...helpers import validate_directories_files,validate_response,validate_downloaded_content,update_aws_credentials
+from ...utils import csv_util,gpkg_util,txt_file_validation_util
+
+# @pytest.mark.skip(reason="skipping this test for now")
+
+def test_fim_c_TP_A_EVAL_subcase2(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,read_csv,fetch_data_file_details,fetch_txt_details,fetch_directory_details,remove_file):
+    tn = test_name + '.json'
+    folder_name = 'PI7/data'
+    
+    directory = fetch_directory_details(folder_name,tn,scenarios)
+    docker_commands,dir_paths = fetch_docker_details(folder_name,tn,scenarios)
+     
+    for i,docker_command in enumerate(docker_commands):
+         if i==2:
+           docker_command=update_aws_credentials(docker_command)
+         run_docker_script(folder_name,tn,scenarios,docker_command)
+         print("The command ran successfully.")
+    text,dir_paths = fetch_txt_details(folder_name,tn,scenarios)
+    for txt, dir_path in zip(text,[os.path.expanduser(each_dir_path_file) for each_dir_path_file in dir_paths ]):
+          txt_file_validation_util.txt_file_validation(txt,dir_path)
+    remove_file(folder_name,tn,os.path.expanduser(directory),scenarios)
